0344e8fc6e6a3bbc03ca5f5625c4c6d2be0f52ad
Mariya Zhurova <mzhurova4@mail.ru>
2024-01-05 00:56:03 +0700

    add

--- tests/api/test_get_list.py
+++ tests/api/test_get_list.py
@@ -9,43 +9,63 @@ from allure_commons._allure import step
 from allure_commons.types import AttachmentType
 
 
-def test_get_list_country_and_city(api_url):
-    url = f'{api_url}/api/v1/cities/list'
-
-    result: Response = requests.get(url)
-
-    assert result.status_code == 200
-
+def boxberry_api_get(url, **kwargs):
     with step("API Request"):
-        result = requests.get(url)
+        result = requests.get(f'{url}', **kwargs)
+
         allure.attach(body=result.request.url, name="Request url",
                       attachment_type=AttachmentType.TEXT)
         allure.attach(body=json.dumps(result.request.body, indent=4, ensure_ascii=True), name="Request body",
                       attachment_type=AttachmentType.JSON, extension="json")
         allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response",
                       attachment_type=AttachmentType.JSON, extension="json")
-        logging.info(result.request.url)
-        logging.info(result.status_code)
-        logging.info(result.text)
+
+        logging.info("Request: " + result.request.url)
+        if result.request.body:
+            logging.info("INFO Request body: " + result.request.body)
+        logging.info("Request headers: " + str(result.request.headers))
+        logging.info("Response code " + str(result.status_code))
+        logging.info("Response: " + result.text)
+    return result
+
+
+def test_get_list_country_and_city(api_url):
+    url = f'{api_url}/api/v1/cities/list'
+
+    # result: Response = requests.get(url)
+    result = boxberry_api_get(f'{url}')
+    assert result.status_code == 200
+
+    # with step("API Request"):
+    #     result = requests.get(url)
+    #     allure.attach(body=result.request.url, name="Request url",
+    #                   attachment_type=AttachmentType.TEXT)
+    #     allure.attach(body=json.dumps(result.request.body, indent=4, ensure_ascii=True), name="Request body",
+    #                   attachment_type=AttachmentType.JSON, extension="json")
+    #     allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response",
+    #                   attachment_type=AttachmentType.JSON, extension="json")
+    #     logging.info(result.request.url)
+    #     logging.info(result.status_code)
+    #     logging.info(result.text)
 
 
 def test_get_daily(api_url):
     url = f'{api_url}/api/v1/cbr/daily'
     schema = load_schema("response_daily.json")
 
-    result: Response = requests.get(url)
-
+    # result: Response = requests.get(url)
+    result = boxberry_api_get(f'{url}')
     assert result.status_code == 200
 
     jsonschema.validate(result.json(), schema)
 
-    with step("API Request"):
-        result = requests.get(url)
-        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response",
-                      attachment_type=AttachmentType.JSON, extension="json")
-        logging.info(result.request.url)
-        logging.info(result.status_code)
-        logging.info(result.text)
+    # with step("API Request"):
+    #     result = requests.get(url)
+    #     allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response",
+    #                   attachment_type=AttachmentType.JSON, extension="json")
+    #     logging.info(result.request.url)
+    #     logging.info(result.status_code)
+    #     logging.info(result.text)
 
 
 def test_get_city(api_url):
@@ -54,10 +74,14 @@ def test_get_city(api_url):
     url = f'{api_url}/api/v1/odp?'
     schema = load_schema("response_city.json")
 
-    result = requests.get(
+    result = boxberry_api_get(
         url=url,
         params={"cityCode": cityCode, "perPage": perPage}
     )
+    # result = requests.get(
+    #     url=url,
+    #     params={"cityCode": cityCode, "perPage": perPage}
+    # )
     assert result.status_code == 200
     # assert result.json()['cityCode'] == cityCode
     jsonschema.validate(result.json(), schema)
